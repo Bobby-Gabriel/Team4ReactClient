7c2f42a5527ad9aa67938f74a8866cac32d376eb
Bobby Gabriel <gabriel_robert@bah.com>
2023-08-11 13:29:47 -0400

    changed to aws urls

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
 
-    app.use(proxy('/api', { target: 'http://gateway:8080/' }));
-    app.use(proxy('/account', { target: 'http://auth:8081/' }));
+    app.use(proxy('/api', { target: 'http://3.143.227.193:8080/' }));
+    app.use(proxy('/account', { target: 'http://18.222.240.95:8081/' }));
 	
 	/*
 	app.use(proxy('/api', { target: 'http://172.18.0.2:8080/' }));
